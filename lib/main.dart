c6349870c9b7a1a60a777c902a72a6157a27af6c
feyto81 <feyto81@gmail.com>
2022-07-03 22:09:03 +0700

    uncomment because use anonymous method

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String message = "Ini adalah Text";
 
-  void tombolDitekan() {
-    setState(() {
-      message = "Tombol sudah ditekan";
-    });
-  }
+  // void tombolDitekan() {
+  //   setState(() {
+  //     message = "Tombol sudah ditekan";
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
